3c2590d9e88a1cdf976a8c2b2da33b2695062515
DeepakPC <ainutiwari483@gmail.com>
2023-10-24 20:23:09 +0530

    first commit

---
+++ src/Component/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assests/images/logo.png';
+const Logo = () => {
+  return (
+    <>
+      <img src={logo} alt={'lawfirm logo'} />
+      <p>IGSTUDIO</p>
+    </>
+  );
+};
+
+export default Logo;
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from '../Logo/Logo';
+const Navbar = () => {
+  return (
+    <>
+      <Logo />
+      <Navigation />
+      <Contact />
+      
+    </>
+  );
+};
+
+export default Navbar;
